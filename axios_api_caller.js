5d6c9fcd1087deb79f81cf0dec90ce5a621560ed
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2021-11-02 22:02:42 +0500

    fixed post

--- axios_api_caller.js
+++ axios_api_caller.js
@@ -1,8 +1,28 @@
 const getUsers = () => {
-    const axios_get_call = axios.get('https://user-api-aunsyedshah.herokuapp.com/api/users');
-    axios_get_call.then(response => {
-        document.getElementById('result').innerHTML = response.data.map(user => `<li>${user.name}</li>`).join('');
-    }).catch(error => {
-        return error;
-    });
+    const axios_get_call = axios.get
+        ('https://user-api-aunsyedshah.herokuapp.com/api/users');
+    axios_get_call.then
+        (
+            response => {
+                document.getElementById('result').innerHTML = response.data.map(user => `<li>${user.name}</li>`).join('');
+            }).catch(error => {
+                return error;
+            });
+}
+
+const postUsers = () => {
+    const axios_post_call = axios.post
+        (
+            'https://user-api-aunsyedshah.herokuapp.com/api/adduser',
+            {
+                name: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            }
+        ).then
+        (
+            response => {
+                console.log(response);
+            }
+        );
 }
