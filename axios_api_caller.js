29ab83e637d88d19f93514863d430fcfb09dc95c
Syed Aun Muhammad <aunsyedsyed@gmail.com>
2021-11-02 19:38:56 +0500

    add feature for post form

--- axios_api_caller.js
+++ axios_api_caller.js
@@ -2,7 +2,6 @@ const getUsers = () => {
     const axios_get_call = axios.get('https://user-api-aunsyedshah.herokuapp.com/api/users');
     axios_get_call.then(response => {
         document.getElementById('result').innerHTML = response.data.map(user => `<li>${user.name}</li>`).join('');
-        return response.data;
     }).catch(error => {
         return error;
     });
